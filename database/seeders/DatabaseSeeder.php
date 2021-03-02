5872aed7193c8227b68eb4a9b997db5678fa2289
Gregory Steenhagen <gregorysteenhagen@Gregorys-MacBook-Pro.local>
2021-03-02 10:44:00 -0500

    Added production check in seeder so production site doesnt have fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Database\Seeders\Traits\TruncateTable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
 
 /**
  * Class DatabaseSeeder.
@@ -26,12 +27,14 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call(AuthSeeder::class);
-        $this->call(AnnouncementSeeder::class);
-        $this->call(ProjectsTableSeeder::class);
-        $this->call(BlogsTableSeeder::class);
-        $this->call(ThemesTableSeeder::class);
-        $this->call(VisitsTableSeeder::class);
-        $this->call(PositionsTableSeeder::class);
+        if (! App::environment('production')) {
+            $this->call(AnnouncementSeeder::class);
+            $this->call(ProjectsTableSeeder::class);
+            $this->call(BlogsTableSeeder::class);
+            $this->call(ThemesTableSeeder::class);
+            $this->call(VisitsTableSeeder::class);
+            $this->call(PositionsTableSeeder::class);
+        }
 
         Model::reguard();
     }
