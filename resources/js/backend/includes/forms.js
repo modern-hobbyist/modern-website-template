b24b6789f3759f068fc7b1067318c0afa12e8d25
csteamengine <csteen1005@gmail.com>
2021-03-04 09:42:31 -0500

    removed console log statement

--- resources/js/backend/includes/forms.js
+++ resources/js/backend/includes/forms.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
     $('#page_content').summernote();
 
     $('.square').on('click', function(){
-        console.log($(this).data('trigger'));
         $($(this).data('trigger')).click();
     })
 })
